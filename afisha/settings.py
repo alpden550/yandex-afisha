5d2acb46878aa61c5a3144367142006f6a877578
alpden550 <alpden550@gmail.com>
2020-06-03 19:17:03 +0300

    Updated settings for static files.

--- afisha/settings.py
+++ afisha/settings.py
@@ -125,9 +125,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, "static"),
 ]
-STATIC_ROOT = ''
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 # Media settings
 
