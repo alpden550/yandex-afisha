352087541412cff3f6778b0f7a89609cb3399d8c
alpden550 <alpden550@gmail.com>
2020-06-03 18:55:19 +0300

    Updated secret key to publish on pythonanywhere.

--- afisha/settings.py
+++ afisha/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env.str('SECRET_KEY')
+SECRET_KEY = 'oeepmc=9r68h%xk70lx^j00x5(0g$+lbi3sej73%lx*0gccwt^'  # public for pythonanywhere
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool('DEBUG')
