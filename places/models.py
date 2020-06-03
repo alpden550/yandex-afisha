4ba60d38acad8b230b39c19d41cec0509b9d6de6
alpden550 <alpden550@gmail.com>
2020-06-03 10:53:36 +0300

    Added model Place.

--- places/models.py
+++ places/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Place(models.Model):
+    """Model to represent place object."""
+
+    title = models.CharField('Title', max_length=250, unique=True, db_index=True)
+    description_short = models.TextField('Short description', blank=True)
+    description_long = models.TextField('Full description', blank=True)
+    latitude = models.FloatField('Latitude')
+    longitude = models.FloatField('Longitude')
+
+    class Meta:
+        verbose_name = 'Place'
+        verbose_name_plural = 'Places'
+
+    def __str__(self):
+        return self.title
