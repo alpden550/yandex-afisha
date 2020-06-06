e07cbda807c99806b3cdbe6ee1b009fe9e62cb36
alpden550 <alpden550@gmail.com>
2020-06-06 16:24:23 +0300

    Added image preview for images in place object.

--- places/admin.py
+++ places/admin.py
@@ -1,10 +1,22 @@
 from django.contrib import admin
+from django.utils.html import format_html
+from django.utils.safestring import mark_safe
 
-from places.models import Place, Image
+from places.models import Image, Place
 
 
 class ImageInline(admin.TabularInline):
     model = Image
+    readonly_fields = ('image_prewiew',)
+    list_display = ('title', 'image_prewiew',)
+
+    def image_prewiew(self, obj):
+        html = '<img src="{url}" width="{width}" height={height} />'.format(
+            url=obj.image.url,
+            width=obj.image.width / 6,
+            height=obj.image.height / 6,
+        )
+        return format_html('{}', mark_safe(html))
 
 
 @admin.register(Place)
